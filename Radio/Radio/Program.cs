684d867842d9d47e30d2ee84517067a8690c7bc0
fakuf <faku_f@hotmail.com>
2017-04-26 08:14:10 -0300

    final

---
+++ Radio/Radio/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radio
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamReader reader = File.OpenText("C:/Users/Facu/Desktop/facu/radio/radios/radiosNacionales.txt");
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string cleanedString = System.Text.RegularExpressions.Regex.Replace(line, @"\s+", " ");
+                    string[] stringWithOneBlank = cleanedString.Split(' ');
+                    string radioName="";
+                    for (int i = stringWithOneBlank.Length - 1; i > 0; i--)
+                    {
+                        radioName += stringWithOneBlank[i];
+                    }
+                    if (args.Length > 0) {
+                        string parameter = args[0].ToLower();
+                        radioName = radioName.ToLower();
+                        if (radioName.Contains(parameter))
+                        {
+                            Process.Start("C:/Program Files (x86)/VideoLAN/VLC/vlc", stringWithOneBlank[0]);
+                        }
+                    }
+                }
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
